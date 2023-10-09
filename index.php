a1adcc2e8069aa9e4e2ea1aef69af0f677844609
ID Lean <122412925+idlearnNaN@users.noreply.github.com>
2023-10-09 15:46:07 +0000

    ma liste de tâche

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+  if(!empty ($_POST['tache'])){
+     $tache=$_POST['tache'];
+
+   $connexion= mysqli_connect('localhost','root','','todoliste');
+
+   if($connexion){
+    echo "connexion reusi";
+   }
+
+    $envoie="INSERT INTO tâche(tache)";
+    $envoie .="VALUES('$tache')";
+
+    $requette=mysqli_query($connexion,$envoie);
+
+    if($requette){
+        echo "ok";
+    }else{
+        echo "non";
+    }
+
+    $affiche="SELECT * FROM tâche";
+    $execute=mysqli_query($connexion, $affiche);
+
+    if($affiche){
+        echo"yes";
+      }else{
+        echo"nooooo";
+      }
+
+      $afficheTache=mysqli_fetch_all($execute, MYSQLI_ASSOC);
+        var_dump($afficheTache);
+     
+  }
+  
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TodoListe</title>
+</head>
+<body>
+   
+    <style>
+
+      .todoliste{
+        margin-top:10px
+        
+      }
+  
+        table{
+       border-collapse:collapse;
+       width:50%
+        }
+        th,td{
+  border:1px solid;
+        }
+    </style>
+     
+   <section>
+    <h1>Mes tâches</h1>
+    <form action="" method="post">
+    <input type="text" name="tache" id="tache">
+    <input type="submit" value="Ajouter">
+    </form>
+   </section>
+
+   <div class="todoliste">
+
+      <table>
+        <tr>
+           <th>Tâche</th>
+           <th colspan="2">Action</th>
+      
+        </tr>
+        <?php foreach($afficheTache as $value):?>
+        <tr>
+            <td><?php echo $value['tache'] ?></td>
+            <td><a href="./modofier.php">modifier</a></td>
+            <td><a href="">suprimer</a></td>
+        </tr>
+        <?php endforeach;?>
+      </table>
+   </div>
+
+</body>
+</html>
---
+++ modofier.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+    <h1>Modifier la tâche</h1>
+    <form action="" method="post">
+    <input type="text">
+    <input type="submit" value="modifier">
+   </form>
+    </section>
+</body>
+</html>
